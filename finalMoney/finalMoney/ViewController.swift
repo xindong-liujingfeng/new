b3d733cc6021d51fdee13dc84812f48d6d496bbc
+ <523163731@qq.com>
2016-07-25 15:01:16 +0800

    初始化项目

---
+++ finalMoney/finalMoney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  finalMoney
+//
+//  Created by 刘景峰 on 16/7/25.
+//  Copyright © 2016年 L_J_F. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
